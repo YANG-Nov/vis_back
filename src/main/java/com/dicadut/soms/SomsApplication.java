9300f34852aa1114baf1a357a8f852e08a8e72f3
weilei <weilei@wusongtech.com>
2021-07-04 18:26:59 +0800

    refactor

--- src/main/java/com/dicadut/soms/SomsApplication.java
+++ src/main/java/com/dicadut/soms/SomsApplication.java
@@ -12,23 +12,6 @@ public class SomsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SomsApplication.class, args);
-
-        int orderNumber = 11;
-        System.out.println("第" +  orderNumber + "个数是：" + fibonacci( orderNumber));
-
-
-    }
-
-    public static int fibonacci1(int orderNumber) {
-        if ((0 == orderNumber ) || (1 == orderNumber))
-            return orderNumber;
-        else
-            return fibonacci1(orderNumber - 1) + fibonacci1(orderNumber - 2);
-    }
-
-    public static int fibonacci(int orderNumber){
-        orderNumber--;
-        return fibonacci1(orderNumber);
     }
 
 }
---
+++ src/main/java/com/learn/sxc/Fibonacci.java
@@ -0,0 +1,35 @@
+package com.learn.sxc;
+
+/**
+ * @author Radium
+ * @version 1.0
+ * @date 2021-07-04 17:51:58
+ */
+public class Fibonacci {
+    public static long fibonacci1(long n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fibonacci1(n - 2) + fibonacci1(n - 1);
+    }
+
+    public static long fibonacci2(long n) {
+        if (n < 2)
+            return n;
+        long a = 0, b = 1;
+        long num = 0;
+        for (int i = 1; i < n; i++) {
+            num = a + b;
+            a = b;
+            b = num;
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci1(100));
+        System.out.println(fibonacci2(100));
+    }
+
+}
