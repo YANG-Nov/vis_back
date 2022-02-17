ec0d5fe12656063fb69451e363d00f7a84dab777
weilei <weilei@wusongtech.com>
2022-02-17 14:25:49 +0800

    add StakeNumberSerializer and StakeNumberDeserializer

--- src/main/java/com/dicadut/soms/dto/StakeNumberDTO.java
+++ src/main/java/com/dicadut/soms/dto/StakeNumberDTO.java
@@ -1,5 +1,9 @@
 package com.dicadut.soms.dto;
 
+import com.dicadut.soms.json.StakeNumberDeserializer;
+import com.dicadut.soms.json.StakeNumberSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 
 /**
@@ -10,5 +14,8 @@ import lombok.Data;
 @Data
 public class StakeNumberDTO {
     private String id;
+
+    @JsonSerialize(using = StakeNumberSerializer.class)
+    @JsonDeserialize(using = StakeNumberDeserializer.class)
     private String number;
 }
