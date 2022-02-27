27fd38e5f621ec2bd7797c9465358cc7b06dd2f6
quxiaoyang <61316581@qq.com>
2022-02-27 20:29:20 +0800

    update addDiseaseRecord

--- src/main/java/com/dicadut/soms/domain/DiseaseRecord.java
+++ src/main/java/com/dicadut/soms/domain/DiseaseRecord.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.*;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import io.swagger.annotations.ApiModelProperty;
@@ -61,9 +62,9 @@ public class DiseaseRecord extends Model<DiseaseRecord> {
 
     @ApiModelProperty(value = "创建时间")
     @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
+    private LocalDateTime createTime;
 
     @ApiModelProperty(value = "更新时间")
     @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
+    private LocalDateTime updateTime;
 }
